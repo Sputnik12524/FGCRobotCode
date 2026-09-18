7e2028dc3494a98406b844a6e478a2d44aee909b
Dashaa <d.smirnova@lab244.ru>
2026-09-18 20:43:01 +0300

    Drive Train. Надо сделать на Omni

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/DriveTrain.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.modules;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class DriveTrain {
+
+    private DcMotor leftFront, leftBack, rightFront, rightBack;
+    LinearOpMode linearOpMode;
+
+    public DriveTrain(LinearOpMode aggregate) {
+        linearOpMode = aggregate;
+        leftFront = linearOpMode.hardwareMap.get(DcMotor.class, "leftFront");
+        leftBack = linearOpMode.hardwareMap.get(DcMotor.class, "leftBack");
+        rightFront = linearOpMode.hardwareMap.get(DcMotor.class, "rightFront");
+        rightBack = linearOpMode.hardwareMap.get(DcMotor.class, "rightBack");
+
+        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void setPowerMecanum(double main, double side, double rotation) {
+
+    }
+}
